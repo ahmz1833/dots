d44d6755ec18bb4fe5735051e5677f2606929761
AmirHossein MohammadZadeh <amir.mohammadzadeh@divar.ir>
2026-03-06 21:18:38 +0330

    Fix S3 bucket path and improve tarball creation in publish script

--- publish.sh
+++ publish.sh
@@ -2,24 +2,28 @@
 
 set -e
 
-S3_BUCKET="s3://ahmz"
-DOTS_DIR="dots"
+S3_BUCKET="s3://ahmz/dots"
 
-cd ..
+echo "Creating tarball of the current repository..."
+TEMP_DIR=$(mktemp -d)
+cp -R . "${TEMP_DIR}/dots"
 
-echo "Creating tarball of the dots directory..."
-tar -czf dots.tar.gz "$DOTS_DIR"
+# Clean up redundant files before archiving
+rm -rf "${TEMP_DIR}/dots/.git" "${TEMP_DIR}/dots/dots.tar.gz"
+
+tar -czf dots.tar.gz -C "$TEMP_DIR" dots
+rm -rf "$TEMP_DIR"
 
 echo "Uploading dots.tar.gz to S3..."
-s3cmd put dots.tar.gz "${S3_BUCKET}/dots/dots.tar.gz" --acl-public
-
-cd dots
+s3cmd put dots.tar.gz "${S3_BUCKET}/dots.tar.gz" --acl-public
 
 echo "Preparing forced-S3 version of bootstrap.sh..."
 sed 's/FETCH_MODE="interactive"/FETCH_MODE="s3"/' bootstrap.sh > /tmp/bootstrap_s3.sh
 
 echo "Uploading bootstrap.sh to S3..."
-s3cmd put /tmp/bootstrap_s3.sh "${S3_BUCKET}/dots/bootstrap.sh" --acl-public
+s3cmd put /tmp/bootstrap_s3.sh "${S3_BUCKET}/bootstrap.sh" --acl-public
+# Install with:
+# curl -sL https://s3.ahmz.ir/dots/bootstrap.sh | bash
 
 rm dots.tar.gz
 rm /tmp/bootstrap_s3.sh
